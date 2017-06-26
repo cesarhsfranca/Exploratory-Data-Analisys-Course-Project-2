92f43435f510e1ee64bb595f19c6bff496fb7627
cesarhsfranca <cesarhsfranca@gmail.com>
2017-06-25 21:33:21 -0300

    Add files via upload

---
+++ plot_3.R
@@ -0,0 +1,27 @@
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Subset NEI data by Baltimore's fip.
+baltimoreNEI <- NEI[NEI$fips=="24510",]
+
+# Aggregate using sum the Baltimore emissions data by year
+aggTotalsBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
+
+# Generate the png file
+png("plot_3.png",width=480,height=480,units="px",bg="transparent")
+
+# Load ggplot2
+library(ggplot2)
+
+# Name the title, x and y axis of the plot
+ggp <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+        geom_bar(stat="identity") +
+        theme_bw() + guides(fill=FALSE)+
+        facet_grid(.~type,scales = "free",space="free") + 
+        labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+        labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+print(ggp)
+
+dev.off()
